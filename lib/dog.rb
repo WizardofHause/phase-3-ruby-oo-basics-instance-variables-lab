4bf1217279c661e9336b7797c8b9a1fcf6de4966
WizardofHause <jonrhause@gmail.com>
2022-11-03 19:59:12 -0600

    commit tree 7888365dea93225480d00804bf9d3dcf86fa6efc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+    # vvv setter instance method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    # vvv getter instance method
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
